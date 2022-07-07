2d4c243eb50ad655050d1947a52fb6a1ad5b5ec5
Raxephion <Raxephion@gmail.com>
2022-07-07 19:16:37 +0200

    Add files via upload

---
+++ Mult_user_inputs.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul  7 19:10:02 2022
+
+@author: Pierre-Henri Rossouw
+
+60 Apps in 60 Days Challenge - App #13: Multiple User Inputs
+"""
+
+while True:
+    reply = input("Enter Text: ")
+    if reply == 'stop': break
+    print(reply)
